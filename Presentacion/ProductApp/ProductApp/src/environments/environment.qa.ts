26f36eb668b28ff918784a0d19271b3ff2d5ef39
unknown <alestat1@hotmail.com>
2025-05-05 01:55:51 -0600

    HTTPS

--- Presentacion/ProductApp/ProductApp/src/environments/environment.qa.ts
+++ Presentacion/ProductApp/ProductApp/src/environments/environment.qa.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
     qaUrl: 'http://192.168.100.5:3200',
-    apiUrl: 'http://192.168.100.5:3090'
+    apiUrl: 'https://192.168.100.5:3090'
   };
   
