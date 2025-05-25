6fe50aaee0140888fd0e943d970cd25e43c81dc1
unknown <alestat1@hotmail.com>
2025-05-25 13:16:41 -0600

    Update

--- ProductsMangementAPI/Program.cs
+++ ProductsMangementAPI/Program.cs
@@ -91,7 +91,6 @@ app.MapPost("/login", async (LoginModel login, ISpProductRepository SpProductRep
     return Results.Unauthorized();
 });
 
-//Comment added to test
 app.MapGet("/products/validate", (HttpRequest request) =>
 {
     var token = request.Cookies["auth_token"];
